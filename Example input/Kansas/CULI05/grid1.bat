ddcac00395a2f00cdf1035854cdda616a6a99965
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2023-06-14 12:30:28 -0400

    removed subfolder with input files for Kansas

--- Example input/Kansas/CULI05/grid1.bat
+++
@@ -1,6 +0,0 @@
-Y:\ExcelInterface\CreateSoilFIles\CreateSoilFiles.exe "Y:\ExcelInterface\Example input\Kansas\CULI05\CULI05.lyr" /GN CULI05 /SN CULI05
-del output
-del element_elm
-del grid_bnd
-del datagen2.dat
-Dir  *.*  >dir.txt
